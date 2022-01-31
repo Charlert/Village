b822785c31ff3d6eed45898c9066895c8886486a
Charlert <charlert202111@gamil.com>
2022-01-31 17:00:17 +0800

    添加清单文件

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,61 +4,67 @@ import charlert.village.god.Staff
 import net.sourceforge.argparse4j.helper.HelpScreenException
 import net.sourceforge.argparse4j.inf.ArgumentParserException
 
-fun welcome() {
-    println("Welcome to the Village of Charlert!")
-    println("Enter 'help' for a list of commands.")
-    println("Enter 'quit' to exit the game.")
-    println()
-}
+class Main {
+    companion object {
+        private fun welcome() {
+            println("Welcome to the Village of Charlert!")
+            println("Enter 'help' for a list of commands.")
+            println("Enter 'quit' to exit the game.")
+            println()
+        }
 
-fun Staff.help() {
-    try {
-        let(arrayOf("--help"))
-    } catch (e: ArgumentParserException) {
-        println(e.message)
-    }
-    repeat(16) {
-        print("-")
-    }
-    println()
-    try {
-        plz(arrayOf("--help"))
-    } catch (e: ArgumentParserException) {
-        println(e.message)
-    }
-    repeat(16) {
-        print("-")
-    }
-    println()
-    try {
-        summon(arrayOf("--help"))
-    } catch (e: ArgumentParserException) {
-        println(e.message)
-    }
-}
-
-fun main() {
-    welcome()
-    val staff = Staff()
-    while (true) {
-        staff.go()
-        staff.world.draw()
-        println()
-        print("--> ")
-        val userInput = readln().split(" ").toTypedArray()
-        try {
-            when (userInput[0]) {
-                "let" -> staff.let(userInput.drop(1).toTypedArray())
-                "plz" -> staff.plz(userInput.drop(1).toTypedArray())
-                "summon" -> staff.summon(userInput.drop(1).toTypedArray())
-                "help" -> staff.help()
-                "test" -> staff.test()
-                "quit" -> return
-                else -> println("invalid command")
+        private fun Staff.help() {
+            try {
+                let(arrayOf("--help"))
+            } catch (e: ArgumentParserException) {
+                println(e.message)
             }
-        } catch (_: HelpScreenException) {
-        } catch (e: ArgumentParserException) {
-            println(e.message)
+            repeat(16) {
+                print("-")
+            }
+            println()
+            try {
+                plz(arrayOf("--help"))
+            } catch (e: ArgumentParserException) {
+                println(e.message)
+            }
+            repeat(16) {
+                print("-")
+            }
+            println()
+            try {
+                summon(arrayOf("--help"))
+            } catch (e: ArgumentParserException) {
+                println(e.message)
+            }
+        }
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            welcome()
+            val staff = Staff()
+            while (true) {
+                staff.go()
+                staff.world.draw()
+                println()
+                print("--> ")
+                val userInput = readln().split(" ").toTypedArray()
+                try {
+                    when (userInput[0]) {
+                        "let" -> staff.let(userInput.drop(1).toTypedArray())
+                        "plz" -> staff.plz(userInput.drop(1).toTypedArray())
+                        "summon" -> staff.summon(userInput.drop(1).toTypedArray())
+                        "help" -> staff.help()
+                        "test" -> staff.test()
+                        "quit" -> return
+                        else -> println("invalid command")
+                    }
+                } catch (_: HelpScreenException) {
+                } catch (e: ArgumentParserException) {
+                    println(e.message)
+                }
+            }
         }
     }
 }
+
